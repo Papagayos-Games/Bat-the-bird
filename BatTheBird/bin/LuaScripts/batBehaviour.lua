eb3a36123688d591e7445a7c8b330e4f3b7748d3
PabloRBGM <prodra02@ucm.es>
2021-05-24 20:00:45 +0200

    ADD: la camara sigue al jugador en Y
    - tambien se puede resetear el bateo haciendo click derecho

--- BatTheBird/bin/LuaScripts/batBehaviour.lua
+++ BatTheBird/bin/LuaScripts/batBehaviour.lua
@@ -10,7 +10,7 @@ batBehaviour["instantiate"] = function (params, entity)
     self.transform = nil
     self.rb = nil
     self.startPos = nil
-    self.maxStrength = p.maxStrength
+    self.strength = p.strength
     self.time = 0
     self.prevTime = 0
     self.sweetspot = p.sweetspot
@@ -26,42 +26,42 @@ batBehaviour["start"] = function (_self, lua)
     _self.prevTime = lua:getInputManager():getTicks()
 end
 
-batBehaviour["update"] = function (_self, lua)
+batBehaviour["update"] = function (_self, lua, deltaTime)
     local input = lua:getInputManager()
 
-    local delta = input:getTicks() - _self.prevTime
-    _self.time = _self.time + (delta/1000)
-    _self.prevTime = input:getTicks()
+    _self.time = _self.time + deltaTime
 
-    if((not _self.batted) and input:mouseButtonPressed() == 1) then
-        local strength = _self.maxStrength * (_self.sweetspot - (math.abs(_self.sweetspot - _self.time)))
-        _self.rb:addForce1(Vector3(0, 1000 * strength, 0), Vector3(0,0,0), 1)
-        _self.batted = true
-        print(strength)
+    if _self.time > 2.5 and _self.time < 3.5 then
+         print("ahora")
     end
 
+    if((not _self.batted) and input:mouseButtonPressed() == 1) then
+        local strength = _self.strength * (_self.sweetspot - (math.abs(_self.sweetspot - _self.time)))
+        _self.rb:addForce1(Vector3(0, strength, 0), Vector3(0,0,0), 1)
+        local cameraFollow = lua:getLuaSelf(lua:getEntity("defaultCamera"), "followTarget")
+        cameraFollow.setFollow(true)
+        _self.batted = true
+    end
 
-    if(_self.transform:getPosition().y < 0.0) then
+    -- Para debuggear resetea la posicion si se hace click derecho
+    if(input:mouseButtonPressed() == 2) then
         _self.rb:setLinearVelocity(Vector3(0,0,0))
-        print(_self.startPos.y)
         _self.rb:setPosition(Vector3(_self.startPos.x, _self.startPos.y, _self.startPos.z))
         _self.time = 0;
         _self.batted = false
+        local cameraTransform = lua:getTransform(lua:getEntity("defaultCamera"))
+        cameraTransform:setPosition(Vector3(0, 80, 300))
+        local cameraFollow = lua:getLuaSelf(lua:getEntity("defaultCamera"), "followTarget")
+        cameraFollow.setFollow(false)
     end
 end
 
 
-batBehaviour["onCollisionEnter"] = function(_self, lua, otherRb)
-    print("CollisionEnter")
+-- Si toca el suelo no puede batear mas
+batBehaviour["onCollisionEnter"] = function(_self, lua, other)
+    if other:getName() == "floor" then 
+        _self.batted = true
+    end
 end
-
-batBehaviour["onCollisionStay"] = function(_self, lua, otherRb)
-    print("CollisionStay")
-end
-
-batBehaviour["onCollisionExit"] = function(_self, lua, otherRb)
-    print("CollisionExit")
-end
-
 
 return batBehaviour
